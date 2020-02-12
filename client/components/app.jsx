b9806bf6323ad622f450eef336adb78b7310e7a8
James <mrjames8124@gmail.com>
2020-02-11 18:55:49 -0800

    added spotify api

---
+++ client/components/app.jsx
@@ -0,0 +1,5 @@
+// import React from 'React';
+
+// getTopArtist(){
+//   //curl -X "GET" "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDZ1PLW6Dh4kq5BefpsZEbeyfYSEoigmWnkZDEAl6fB5e3MgE47pC2NSkVy_btJsLMBKXIupF0kFAjoCEbYIVgwAFhi_G2fZroNLotLxUi-pVXi9mUTBNPFXdmhiAGEC3PrnsDjKo3BMY2_sUMWSyqm"
+// }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,46 @@
-// require('dotenv/config');
-// const express = require('express');
-// const db = require('./database');
+require('dotenv/config');
+const express = require('express');
+const db = require('./database');
 // const ClientError = require('./client-error');
-// const staticMiddleware = require('./static-middleware');
-// const sessionMiddleware = require('./session-middleware');
+const staticMiddleware = require('./static-middleware');
+const sessionMiddleware = require('./session-middleware');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+  clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+  redirectUri: 'http://www.example.com/callback'
+});
+
+spotifyApi.setAccessToken(`BQDZ1PLW6Dh4kq5BefpsZEbeyfYSEoigmWnkZDEAl6fB5e3MgE47pC2NSkVy_btJsLMBKXIupF0kFAjoCEbYIVgwAFhi_G2fZroNLotLxUi-pVXi9mUTBNPFXdmhiAGEC3PrnsDjKo3BMY2_sUMWSyqm
+`);
+
+const app = express();
+
+app.use(staticMiddleware);
+app.use(sessionMiddleware);
+
+app.use(express.json());
+
+app.get('/api/health-check', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
+// app.get('/api/all'(req, res, next) => {
+
+// })
+
+//  GET https://api.spotify.com/v1/me/top/{type}
+
+app.get('/api/artists', (req, res, next) => {
+
+  spotifyApi.getArtist('45eNHdiiabvmbp4erw26rg')
+    .then(function (data) {
+      // console.log('Artist information', data.body);
+    }, function (err) {
+      console.error(err);
+    });
+
+});
