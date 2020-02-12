bb50aa397ca78b051e108c3fe78e258bbcd62438
James <mrjames8124@gmail.com>
2020-02-11 18:04:22 -0800

    adding file structure

---
+++ client/index.js
@@ -0,0 +1,6 @@
+require('dotenv/config');
+const express = require('express');
+const db = require('./database');
+const ClientError = require('./client-error');
+const staticMiddleware = require('./static-middleware');
+const sessionMiddleware = require('./session-middleware');
