d67832e97bf3d70f18903bb4310c80531156a56b
James <mrjames8124@gmail.com>
2020-02-12 14:27:41 -0800

    finished shell and server is up for the app

--- server/index.js
+++ server/index.js
@@ -34,13 +34,33 @@ app.get('/api/health-check', (req, res, next) => {
 
 //  GET https://api.spotify.com/v1/me/top/{type}
 
-app.get('/api/artists', (req, res, next) => {
+// app.get('/api/artists', (req, res, next) => {
 
-  spotifyApi.getArtist('45eNHdiiabvmbp4erw26rg')
-    .then(function (data) {
-      // console.log('Artist information', data.body);
-    }, function (err) {
-      console.error(err);
+//   spotifyApi.getArtist('45eNHdiiabvmbp4erw26rg')
+//     .then(function (data) {
+//       // console.log('Artist information', data.body);
+//     }, function (err) {
+//       console.error(err);
+//     });
+
+// });
+
+app.use('/api', (req, res, next) => {
+  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
     });
+  }
+});
 
+app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port', process.env.PORT);
 });
